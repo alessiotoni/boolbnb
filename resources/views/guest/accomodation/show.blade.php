e0fcb18bf999d62ef9a87b927d972c48f7f2330b
Giulia-M <giuliamaggio16@gmail.com>
2021-08-01 17:53:24 +0200

    guestShow

--- resources/views/guest/accomodation/show.blade.php
+++ resources/views/guest/accomodation/show.blade.php
@@ -4,20 +4,26 @@
 
 <div class="container">
     
-    <div>
-        <h1 class="title">{{ $accomodation->title }}</h1>
+    <div class="row justify-content-center align-items-center">
+        <div class="wrap col-md-9">
 
-        <h2>{{ $accomodation->description }}</h2>
-        <h2>{{ $accomodation->city }} - {{ $accomodation->streat_name }} - {{ $accomodation->zip }}</h2>
 
-        {{-- @if(isset(Auth::user()->id) && $accomodation->user_id != Auth::user()->id) --}}
-        <a href="{{ route('message.create',['id' => $accomodation->id]) }}">Contatta il proprietario</a>
-        {{-- @endif --}}
+            <h1 class="title">{{ $accomodation->title }}</h1>
+            <p class="city">{{ $accomodation->city }} - {{ $accomodation->street_name }} - {{ $accomodation->zip }}
+            </p>
 
-        
-        <slider-images
-        :id='{{$accomodation->id}}'
-        ></slider-images>
+            <h2 class="description">{{ $accomodation->description }}</h2>
+            
+
+            {{-- @if(isset(Auth::user()->id) && $accomodation->user_id != Auth::user()->id) --}}
+            <a href="{{ route('message.create',['id' => $accomodation->id]) }}">Contatta il proprietario</a>
+            {{-- @endif --}}
+
+            
+            <slider-images
+            :id='{{$accomodation->id}}'
+            ></slider-images>
+        </div>
     </div>
 
 
